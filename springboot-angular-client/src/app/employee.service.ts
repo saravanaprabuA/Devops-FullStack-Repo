16c2592f49187bd8a543d31c817ec3e6300c1b13
saravanaprabuA <saravanaprabu.a@gmail.com>
2020-12-24 18:24:06 +0530

    updated service port

--- springboot-angular-client/src/app/employee.service.ts
+++ springboot-angular-client/src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://192.168.99.100:9999/api/v1/employees';
+  private baseUrl = 'http://192.168.99.100:8888/api/v1/employees';
 
   constructor(private http: HttpClient) { }
 
